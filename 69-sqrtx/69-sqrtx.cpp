50589f5f0e03b7c6a9c39302c9517f4106134d73
Coder Babua <111197936+coderbabuaa@users.noreply.github.com>
2022-08-16 21:19:07 +0530

    Time: 26 ms (13.88%), Space: 5.8 MB (95.25%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        if (x==0||x==1)
+            return x;
+        long int i = 1, result = 1;
+        while (result <= x)
+            {
+                i++;
+                result = i * i;
+            }
+        return i - 1;
     }
 };
