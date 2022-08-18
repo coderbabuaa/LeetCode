aeb3e6c6440457e8ca8ff9fa7739e1767cf18234
Coder Babua <111197936+coderbabuaa@users.noreply.github.com>
2022-08-18 12:00:00 +0530

    Time: 32 ms (15.63%), Space: 18.4 MB (37.02%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -8,11 +8,11 @@ public:
         for (int i = 1; i < nums.size(); ++i) {
             if (nums[i] != nums[last]) {
                 ++last;
-                nums[last] = nums[i];
+                nums[last] = nums[i];//if element is unique gets shifted to initial part of array
             }
         }
 
-        nums.resize(last+1);
+        nums.resize(last+1);//modifies the size of array based on provided index
         return nums.size();
     }
 };
