cd7a256de306567eceaf4f13190258e3de0c3ca2
Coder Babua <111197936+coderbabuaa@users.noreply.github.com>
2022-08-18 23:28:04 +0530

    Time: 6 ms (62.51%), Space: 9.4 MB (22.45%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -4,12 +4,12 @@ public:
        if(strs.size() == 0) return "";
         int ans = INT_MAX;
         for(int i = 0; i < strs.size()-1; i++){
-            ans = min(ans, (int)min(strs[i].size(), strs[i+1].size()));
+            ans = min(ans, (int)min(strs[i].size(), strs[i+1].size()));//find shortest string length
             while(strs[i].substr(0, ans) != strs[i+1].substr(0, ans)){
                 ans--;
             }
-            if(ans == 0)return "";
+            if(ans == 0)return "";//if none of the characters of smallest string match
         }
-        return strs[0].substr(0, ans);
+        return strs[0].substr(0, ans);//display maximum prefix
     }
 };
