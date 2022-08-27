f962754b2790f20a70d69c6995c5ff94e6c7c47d
Coder Babua <111197936+coderbabuaa@users.noreply.github.com>
2022-08-27 21:30:02 +0530

    Time: 2019 ms (5.02%), Space: 28.6 MB (10.30%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_so_far = 0
+        max_ending_here = 0
+       #kadanes algorithm
+        for i in range(0, len(nums)):
+            max_ending_here = max_ending_here + nums[i]
+            if (max_so_far < max_ending_here):
+                max_so_far = max_ending_here
+            if max_ending_here < 0:
+                max_ending_here = 0   
+        if max_so_far>0:
+            return max_so_far
+        else:
+            return max(nums)
