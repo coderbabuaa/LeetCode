1d819caa8a6d8915195cb708fc4c0867eb42fee7
Coder Babua <111197936+coderbabuaa@users.noreply.github.com>
2022-09-01 14:43:52 +0530

    Time: 38 ms (96.84%), Space: 14.1 MB (88.59%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,4 +1,3 @@
 class Solution:
     def isIsomorphic(self, s, t):
-        # use two hash map to store map and reverse map while scanning both lists
         return len(set(zip(s,t)))==len(set(s))==len(set(t))
