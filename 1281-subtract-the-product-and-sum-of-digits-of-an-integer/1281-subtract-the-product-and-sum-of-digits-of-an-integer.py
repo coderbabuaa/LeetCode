a7c374d185a99b86ec872f1d6cd7bff759cf59ad
Coder Babua <111197936+coderbabuaa@users.noreply.github.com>
2022-09-06 16:29:43 +0530

    Time: 66 ms (8.42%), Space: 13.9 MB (53.80%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -1,5 +1,14 @@
-import numpy as np
+from functools import reduce # Since Python 3, reduce() is moved to functools
+
 class Solution:
+    
+    # Helper function for reduce()
+    def product_nums(self,x1,x2):
+        return x1*x2
+
+    
     def subtractProductAndSum(self, n: int) -> int:
-        res = [int(x) for a,x in enumerate(str(n))]
-        return np.prod(np.array(res))-sum(res)
+        digit_chars = list(str(n)) # Get all the characters from the number, after converting it to string
+        digits = list(map(int,digit_chars)) # Getting the digits from number. First converting the chars back to int and then converting map object to list
+        
+        return reduce(self.product_nums,digits) - sum(digits) # Return the diff between product and sum of digits
